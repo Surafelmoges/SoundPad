f0fbeb766b8d2b16c0690dcd959d7d72fbc6dd74
Surafel <surafelmoges01@gmail.com>
2025-06-18 13:01:00 +0100

    Practice Dynamic Style

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import './App.css'
 import paadD from "./pad"
 
-export default function App() {
+export default function App({darkMode}) {
   
   const[pads , setPads] = React.useState(paadD)
+  const styles = {
+   backgroundColor : darkMode ? "#222222" : "blue"
+  }
   const disPad = pads.map(padd=>(
-     <button key={padd.id}></button>
+     <button style={styles} key={padd.id}></button>
 
   ))
 
